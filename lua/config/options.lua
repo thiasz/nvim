89b9b6f8dbefa5f4f61e49599b46f74f178e3a40
Matthias Archut <matthias.archut@deutschebahn.com>
2026-01-13 13:46:48 +0100

    upd options.lua and treesitter.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -34,3 +34,12 @@ vim.o.wrap = false -- disable wrapping
 vim.o.breakindent = true -- prevent line wrapping
 
 vim.diagnostic.config({ virtual_text = false }) -- inline diagnostics
+
+--fix robot and resource comment string
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("FixRobotCommentString", { clear = true }),
+	callback = function(ev)
+		vim.bo[ev.buf].commentstring = "# %s"
+	end,
+	pattern = { "robot", "resource" },
+})
