6a0694be7777e8d59c73e9456e18e02c01fc9e0b
matthias <matthias@matthiass-MacBook-Pro.local>
2026-05-25 14:25:37 +0200

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 vim.pack.add({
-	{ src = "https://github.com/rose-pine/neovim" },
-	{ src = "https://github.com/vague2k/vague.nvim" },
+	-- { src = "https://github.com/rose-pine/neovim" },
+	-- { src = "https://github.com/vague2k/vague.nvim" },
+	-- { src = "https://github.com/ribru17/bamboo.nvim" },
+	-- { src = "https://github.com/ellisonleao/gruvbox.nvim" },
+	-- { src = "https://github.com/f4z3r/gruvbox-material.nvim" },
+	-- { src = "https://github.com/chriskempson/vim-tomorrow-theme" },
+	{ src = "https://github.com/srcery-colors/srcery-vim" },
 })
 
 -- require("rose-pine").setup({
@@ -14,5 +19,9 @@ vim.pack.add({
 -- })
 
 -- vim.cmd("colorscheme rose-pine")
-vim.cmd("colorscheme vague")
+-- vim.cmd("colorscheme vague")
+-- vim.cmd("colorscheme bamboo")
+-- vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme Tomorrow-Night")
+vim.cmd("colorscheme srcery")
 -- vim.cmd(":hi statusline guibg=NONE")
